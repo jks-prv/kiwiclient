d4890bcc7a73a287082dedd274bdb4ca13e6bea2
Rik van Riel <riel@surriel.com>
2026-02-10 16:36:11 -0500

    add camp_wait_event so things work again
    KiwiWorker needs an extra argument nowadays.
    
    Traceback (most recent call last):
      File "/data/source/ham/kiwiclient/./kiwiclientd.py", line 566, in <module>
        main()
        ~~~~^^
      File "/data/source/ham/kiwiclient/./kiwiclientd.py", line 540, in main
        snd_recorders.append(KiwiWorker(args=(KiwiSoundRecorder(opt),opt,True,False,run_event)))
                             ~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/data/source/ham/kiwiclient/kiwi/worker.py", line 17, in __init__
        self._recorder, self._options, self._reader, self._delay_run, self._run_event, self._camp_wait_event = args
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    ValueError: not enough values to unpack (expected 6, got 5)
    
    Add the camp_wait_event argument so things work again.
    
    Signed-off-by: Rik van Riel <riel@surriel.com>

--- kiwiclientd.py
+++ kiwiclientd.py
@@ -515,6 +515,10 @@ def main():
     run_event = threading.Event()
     run_event.set()
 
+    # camp_wait_event is used for camping mode synchronization
+    camp_wait_event = threading.Event()
+    camp_wait_event.set()
+
     options.sdt = 0
     options.dir = None
     options.sound = True
@@ -529,6 +533,9 @@ def main():
     options.wf_cal = None
     options.netcat = False
     options.wideband = False
+    options.admin = False
+    options.password = ''
+    options.tlimit_password = ''
 
     gopt = options
     multiple_connections,options = options_cross_product(options)
@@ -537,7 +544,7 @@ def main():
     for i,opt in enumerate(options):
         opt.multiple_connections = multiple_connections
         opt.idx = i
-        snd_recorders.append(KiwiWorker(args=(KiwiSoundRecorder(opt),opt,True,False,run_event)))
+        snd_recorders.append(KiwiWorker(args=(KiwiSoundRecorder(opt),opt,True,False,run_event,camp_wait_event)))
 
     try:
         for i,r in enumerate(snd_recorders):
